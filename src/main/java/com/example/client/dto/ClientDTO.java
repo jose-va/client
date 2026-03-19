56bd5c4018035fb892c409e94fd31e8825c10b0b
Jose Manuel <trijostyt@gmail.com>
2026-03-19 13:06:09 +0100

    feat: [ClientDTO]: Agregar el DTO del cliente y el mapper

---
+++ src/main/java/com/example/client/dto/ClientDTO.java
@@ -0,0 +1,14 @@
+package com.example.client.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientDTO {
+    private String id;
+    private String name;
+    private String email;
+    private String cifNifNie;
+}
--- src/main/java/com/example/client/mappers/ClientMapper.java
+++ src/main/java/com/example/client/mappers/ClientMapper.java
@@ -1,4 +1,13 @@
 package com.example.client.mappers;
 
-public class ClientMapper {
+import com.example.client.dto.ClientDTO;
+import com.example.client.model.Client;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ClientMapper {
+
+    ClientDTO toDto(Client client);
+
+    Client toEntity(ClientDTO clientDto);
 }
