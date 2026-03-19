3603dee827985a7c313bb6815a1e4017d593107f
Jose Manuel <trijostyt@gmail.com>
2026-03-19 16:27:46 +0100

    fix: [Controller]: Endpoints cambiados para coincidir con los metodos del service

--- src/main/java/com/example/client/controller/ClientController.java
+++ src/main/java/com/example/client/controller/ClientController.java
@@ -2,13 +2,18 @@ package com.example.client.controller;
 
 import com.example.client.dto.ClientDTO;
 import com.example.client.service.ClientService;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
-@RestController
+@Validated
 @RequestMapping ("/api/clients")
+@RestController
 public class ClientController {
 
     private final ClientService clientService;
@@ -16,20 +21,28 @@ public class ClientController {
     @PostMapping("/create")
     @ResponseStatus(HttpStatus.OK)
     public void createClient(@RequestBody ClientDTO dto) {
-
         clientService.saveClient(dto);
     }
 
     @GetMapping("/find/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public ClientDTO getAllClients(@PathVariable String id) {
-        return clientService.findById(id);
+    public Object findById(@PathVariable String id, @RequestParam String nif, @RequestParam boolean simpleOutput) {
+        return clientService.findById(id, nif, simpleOutput);
     }
 
-    @DeleteMapping("/{id}")
+    @GetMapping("/email")
+    public Optional<ClientDTO> findByEmail(@Pattern(regexp = "^[a-z]+\\.[a-z]+@petroprix.com$") @RequestParam String email){
+        return clientService.findByEmail(email);
+    }
+
+    @GetMapping("/name")
+    public List<ClientDTO> findByName(@RequestParam String name){
+        return clientService.findByName(name);
+    }
+
+    @PutMapping("/{id}")
     @ResponseStatus(HttpStatus.OK)
-    public void deleteClient(@PathVariable String id) {
-        clientService.deleteClient(id);
+    public void updateClient(@RequestBody ClientDTO dto, @PathVariable String id){
+        clientService.updateClient(dto, id);
     }
-
 }
--- src/main/java/com/example/client/service/ClientService.java
+++ src/main/java/com/example/client/service/ClientService.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -27,17 +29,47 @@ public class ClientService {
         clientRepository.save(client);
     }
 
-    public ClientDTO findById(String id) {
-        Client client = clientRepository.findById(id);
+    public void updateClient(ClientDTO dto, String id) {
+
+        Client updateClient = clientMapper.toEntity(dto);
+
+        updateClient.setId(id);
+        updateClient.setPk("CLIENT#" + id);
+        updateClient.setSk("NIF#" + updateClient.getCifNifNie());
+        updateClient.setGIndex2Pk("EMAIL#" + updateClient.getEmail());
+
+        clientRepository.save(updateClient);
+    }
+
+    public Object findById(String id, String nif, boolean simpleOutput) {
+
+        String pk = "MERCHANT#" + id;
+        String sk = "NIF#" + nif;
+
+        Client client = clientRepository.findById(pk, sk);
         if (client == null) throw new RuntimeException("Cliente no encontrado");
+        
+        if (simpleOutput) return Map.of("id", client.getId());
 
         return clientMapper.toDto(client);
     }
 
-    public void deleteClient(String id) {
-        Client client = clientRepository.findById(id);
-        if (client == null) throw new RuntimeException("Cliente no encontrado");
+    public List<ClientDTO> findByName(String name) {
 
-        clientRepository.delete(client);
+        List<Client> clientList= clientRepository.findAll();
+
+        return clientList
+                .stream()
+                .filter(c -> c.getName().toLowerCase().contains(name.toLowerCase()))
+                .map(clientMapper::toDto)
+                .toList();
+    }
+
+    public Optional<ClientDTO> findByEmail(String email) {
+        return clientRepository.findAll()
+                .stream()
+                .filter(c -> c.getEmail().toLowerCase().contains(email.toLowerCase()))
+                .map(clientMapper::toDto)
+                .findFirst();
     }
 }
